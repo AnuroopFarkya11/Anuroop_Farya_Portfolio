225062cd4f2bc7832bef6922bd32aca897015ec3
AnuroopFarkya11 <anuroopf02@gmail.com>
2024-12-12 17:01:45 +0530

    Updated Env

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:my_portfolio/app.dart';
-
 void configureApp() {
   setUrlStrategy(PathUrlStrategy());
 }
 
-void main() {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   // usePathUrlStrategy();
+  await dotenv.load();
   runApp(const ProviderScope(child: MyApp()));
 }
